ae98dc2f427008fcaaa1bf7f91ea2d69cbd5683c
Dilan Madusanka <madusankadilan226@gmail.com>
2024-02-12 14:33:41 +0530

    navbar updated

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero"
+import Services from "./components/Services";
+import Footer from "./components/Footter";
+import Recommend from "./components/Recommend";
+import Test from './components/Test';
+
+
+export default function App() {
+  return (
+    <>
+   
+   
+    <Navbar />
+   
+
+    
+    </>
+  )
+}
